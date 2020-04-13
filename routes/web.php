897d28f22e684bbf1ec130be46dce186860d0af8
Hotrak <vladtumen2000@mail.ru>
2020-04-13 20:48:45 +0300

    test 2

--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('/', function (){
 //    $schedule = new Schedule();
 //    dd($schedule->scheduleShortWithCountPlaces(345,'2020-04-12'));
 //    phpinfo();
-    return "123";
+    return "1234";
 });
 //Route::get('/', 'ScheduleController@store');
 Route::get('/33', function (){
