dbc1e38c92619b325e006886f3983e3724a1c049
Hotrak <vladtumen2000@mail.ru>
2020-06-10 15:18:28 +0300

    refix transit points

--- app/Http/Controllers/TownController.php
+++ app/Http/Controllers/TownController.php
@@ -20,12 +20,13 @@ class TownController extends Controller
     public function points($townConnectionId){
         $townConnection = TownConnection::find($townConnectionId);
         if($townConnection->is_transit == true && $townConnection->town_x != 1){
-            $points = $townConnection->town1->points->where('is_transit',1)->get();
+            $points = $townConnection->town1->points->where('is_transit',1);
+//            dd($points);
+
         }
         else{
             $points = $townConnection->town1->points;
         }
-
         return $points;
     }
     public function allPoints($townId){
